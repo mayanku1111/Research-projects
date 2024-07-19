02267aa0ae373fe77328adadf293155d4f9060be
Mayank kumar <138918191+mayanku1111@users.noreply.github.com>
2024-07-20 03:30:27 +0530

    Create web-nav.js

---
+++ web-nav.js
@@ -0,0 +1,39 @@
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Voice Navigation Extension installed');
+});
+
+// content.js
+let recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.onresult = function(event) {
+  let command = event.results[event.results.length - 1][0].transcript;
+  processCommand(command);
+};
+
+function processCommand(command) {
+  // Implement NLP and command execution logic here
+  console.log('Command received:', command);
+}
+
+recognition.start();
+
+// few-shot-learning.js
+class FewShotLearner {
+  constructor() {
+    this.commandMappings = {};
+  }
+
+  learn(command, action) {
+    // Implement learning logic
+    this.commandMappings[command] = action;
+  }
+
+  predict(command) {
+    // Implement prediction logic
+    return this.commandMappings[command] || 'unknown';
+  }
+}
+
+// Integrate with content.js
+let learner = new FewShotLearner();
